8588298e9cc05b2400650c5ec54eff4aa2e617c1
unknown <helsingivan@hotmail.com>
2012-09-06 21:34:37 -0300

    alteracao qualquer

--- src/iniciandocomgit/IniciandoComGit.java
+++ src/iniciandocomgit/IniciandoComGit.java
@@ -14,6 +14,6 @@ public class IniciandoComGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("primeiro");
+        System.out.println("Huhuuu 2");
     }
 }
